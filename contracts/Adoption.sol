f8279ea538a5bf0f442321b1ba18b26f66fad4e3
Frank Sung <ysya33333@gmail.com>
2018-04-01 13:36:28 +0800

    fix indent

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -4,11 +4,11 @@ contract Adoption {
 
     address[16] public adopters;  // 保存領養者的地址
 
-        // 領養寵物
+    // 領養寵物
     function adopt(uint petId) public returns (uint) {
         require(petId >= 0 && petId <= 15);  // 確保id在數組長度內
 
-        adopters[petId] = msg.sender;        // 保存調用這地址 
+        adopters[petId] = msg.sender;        // 保存調用地址 
         return petId;
     }
 
