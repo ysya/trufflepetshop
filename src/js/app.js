5257b946039cc8521aa2550c455c91021f335917
Frank Sung <ysya33333@gmail.com>
2018-04-01 11:41:45 +0800

    ch pic & port

--- src/js/app.js
+++ src/js/app.js
@@ -29,7 +29,7 @@ App = {
       App.web3Provider = web3.currentProvider;
     } else {
       // If no injected web3 instance is detected, fallback to the TestRPC
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
     web3 = new Web3(App.web3Provider);
 
